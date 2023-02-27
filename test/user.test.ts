d1fbed4a9abcbcaf0b1e6261cecc540c6d1cf5b7
Otto Hooper <votton.hooper@gmail.com>
2023-02-27 01:50:51 +0000

    bootleg solution to create a sqlite DB before tests

--- test/user.test.ts
+++ test/user.test.ts
@@ -1,11 +1,5 @@
 import request from "supertest";
 import app from "../src/app";
-import fs from "fs";
-
-// Before running the tests, make a sqlite database and name it "sv.db"
-beforeAll(async () => {
-    fs.writeFileSync("sv.db", "");
-});
 
 describe("GET /auth/steam", () => {
     it("should return 302 Found for redirection", () => {
